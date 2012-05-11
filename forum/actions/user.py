b13937220bac0724bd0a580907a3fb4114ead57b
jordan <jordan@0cfe37f9-358a-4d5e-be75-b63607b5c754>
2012-05-11 00:49:34 +0000

    show link that leads to the badge page, not list of all badges
    git-svn-id: http://svn.osqa.net/svnroot/osqa/trunk@1263 0cfe37f9-358a-4d5e-be75-b63607b5c754

--- forum/actions/user.py
+++ forum/actions/user.py
@@ -164,10 +164,10 @@ class AwardAction(ActionProxy):
         self.user.save()
 
         self.user.message_set.create(message=_(
-                """Congratulations, you have received a badge '%(badge_name)s'. <a href="%(badges_url)s">Find out who has it, too</a>."""
+                """Congratulations, you have received a badge '%(badge_name)s'. <a href="%(badge_url)s">Find out who has it, too</a>."""
         ) % dict(
             badge_name=award.badge.name,
-            badges_url=reverse("badges")))
+            badge_url=award.badge.get_absolute_url()))
 
     def cancel_action(self):
         award = self.award
